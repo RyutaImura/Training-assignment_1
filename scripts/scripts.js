865c9fd32833abd084efaa03b86b56a2d81973fe
Ryuta Imura <r.i.engineer0119@gmail.comgit config --global user.name Ryuta>
2025-01-06 14:43:07 +0900

    Remove all files

--- scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.classList.contains('show')) {
-        menu.classList.remove('show');
-    } else {
-        menu.classList.add('show');
-    }
-});
